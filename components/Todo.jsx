2f35294a0df859aaa6bbd4dc907c0e2a81381e5c
vivalchemy <vivalchemy.social+github@gmail.com>
2024-06-24 19:10:45 +0530

    final commit

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -7,7 +7,7 @@ const Todo = ({ text, status, id, removeTodo }) => {
       <div className={`aspect-square p-3  border-4 rounded-lg ${todoStatus ? "bg-green-500 border-green-500" : "bg-white border-blue-500"}`}>
       </div>
       <div className={`text-black text-xl ${todoStatus ? "line-through" : ""}`} >
-        {text} {id}
+        {text}
       </div >
       <svg className="rounded-md bg-white ml-auto" onClick={() => { removeTodo(id) }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" ><path d="M3 6h18" /><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" /><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" /><line x1="10" x2="10" y1="11" y2="17" /><line x1="14" x2="14" y1="11" y2="17" /></svg>
     </div>
